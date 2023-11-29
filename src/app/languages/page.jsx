e964841e49a3bdab567a94459da0e44967503525
mark <markpackham1@gmail.com>
2023-11-29 14:05:30 +0000

    output languages on Page
    https://youtu.be/49mkK-jr5no?feature=shared&t=1013

--- src/app/languages/page.jsx
+++ src/app/languages/page.jsx
@@ -5,13 +5,30 @@ async function getData() {
     // DB Connect
     const client = await connectToDatabase();
     const db = client.db("programming");
-  } catch (error) {}
+
+    // DB Query for languages
+    const items = await db.collection("languages").find({}).toArray();
+    const res = { items };
+    return res;
+  } catch (error) {
+    throw new Error("Failed to fetch data please try again later.");
+  }
 }
 
-const Page = () => {
+const Page = async () => {
+  const data = await getData();
+
   return (
     <>
-      <div className="container mx-auto mt-8">Hello</div>
+      <div className="container mx-auto mt-8">
+        <ul className="grid grid-cols-4 gap-4 text-center">
+          {data.items.map((item) => (
+            <li key={item._id} className="bg-green-500">
+              <h2>{item.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
