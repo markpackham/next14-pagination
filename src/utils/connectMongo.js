33c9f105cb61dac980f30e314fd6c6c86c2fcabe
mark <markpackham1@gmail.com>
2023-11-29 13:29:20 +0000

    setup db connection
    https://youtu.be/49mkK-jr5no?feature=shared&t=567

---
+++ src/utils/connectMongo.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+const MONGODB_URI = process.env.MONGODB_URI;
+let client = null;
+
+export async function connectToDatabase() {
+  if (client) return client;
+
+  if (!MONGODB_URI) {
+    console.log("No MONGODB_URI");
+  }
+
+  try {
+    client = await MongoClient.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+    return client;
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error);
+  }
+}
